052442b54db41c6b33b746aece0cc515d9e2c336
abraxas93 <savchyk.a@gmail.com>
2017-09-25 17:16:26 +0300

    build simple script for coinmarket cap api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 'use strict';
-
 console.log(process.version);
+
+var CoinMarketCap = require("node-coinmarketcap"),
+    moment = require('moment');
+
+var options = {
+   events: true, // Enable event system
+   refresh: 60, // Refresh time in seconds (Default: 60)
+   convert: "EUR" // Convert price to different currencies. (Default USD)
+}
+var coinmarketcap = new CoinMarketCap(options),
+    i = 0;
+
+setInterval(() => {
+    coinmarketcap.getTop(50, sendToHub);
+    console.log('#iteration: ' + i);
+    i++;
+}, 60000);
+
+
+
+function sendToHub(dataArr) {
+    dataArr.forEach(element => {
+        element.last_updated = moment(element.last_updated*1000).format("YYYY-MM-DD HH:mm:ss");
+        console.log(element);
+    });
+}
